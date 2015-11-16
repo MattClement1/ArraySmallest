81b5d6bd2f45116d44a18a3657727b53919988bf
MattClement1 <matthew.clement1@ocsbstudent.ca>
2015-11-16 12:57:54 -0500

    Smallest Number in Array

---
+++ ArraySmallest/ArraySmallest.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace ArraySmallest
+{
+    partial class frmArraySmallest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClickHere = new System.Windows.Forms.Button();
+            this.lblInstructions = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnClickHere
+            // 
+            this.btnClickHere.Location = new System.Drawing.Point(132, 43);
+            this.btnClickHere.Name = "btnClickHere";
+            this.btnClickHere.Size = new System.Drawing.Size(75, 23);
+            this.btnClickHere.TabIndex = 0;
+            this.btnClickHere.Text = "Click Here";
+            this.btnClickHere.UseVisualStyleBackColor = true;
+            this.btnClickHere.Click += new System.EventHandler(this.btnClickHere_Click);
+            // 
+            // lblInstructions
+            // 
+            this.lblInstructions.AutoSize = true;
+            this.lblInstructions.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInstructions.Location = new System.Drawing.Point(13, 13);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(313, 16);
+            this.lblInstructions.TabIndex = 1;
+            this.lblInstructions.Text = "Click the button to find the smallest value in an array:";
+            // 
+            // frmArraySmallest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(338, 76);
+            this.Controls.Add(this.lblInstructions);
+            this.Controls.Add(this.btnClickHere);
+            this.Name = "frmArraySmallest";
+            this.Text = "Array Smallest";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClickHere;
+        private System.Windows.Forms.Label lblInstructions;
+    }
+}
+
---
+++ ArraySmallest/ArraySmallest.cs
@@ -0,0 +1,64 @@
+﻿/*
+* Created by: Matthew Clement
+* Created on: 16-Nov-2015
+* Created for: Unit #5-03
+* This program outputs the smallest value in an array of random numbers
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArraySmallest
+{
+    public partial class frmArraySmallest : Form
+    {
+        public int SmallestNumber(int[] array)
+        {
+            //This function finds the smallest value in the array
+            int smallestNumber = array[0];
+
+            foreach (int arrayValue in array)
+            {
+                if (arrayValue < smallestNumber)
+                {
+                    smallestNumber = arrayValue;
+                }
+            }
+
+            return smallestNumber;
+        }
+
+        public frmArraySmallest()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClickHere_Click(object sender, EventArgs e)
+        {
+            //Local Variables
+            int[] numArray = new int[15];
+            Random rndNum = new Random();
+            int smallestNum;
+
+            //Generate array values
+            for (int numCounter = 0; numCounter < 15; numCounter++)
+            {
+                int number = rndNum.Next(1, 20 + 1);
+                numArray[numCounter] = number;
+            }
+
+            //Call function
+            smallestNum = SmallestNumber(numArray);
+
+            //Output
+            MessageBox.Show("The smallest number is: " + smallestNum);
+        }
+    }
+}
